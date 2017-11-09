5134f8f72db4db64b300b2b6ae77b0c8db8aaa26
Michael Joyner <joyner.michael@gmail.com>
2017-11-09 10:14:38 +0800

    contact points on contact page

--- resources/views/front/contact/page.blade.php
+++ resources/views/front/contact/page.blade.php
@@ -18,4 +18,14 @@
         <p class="f4 f3-ns col-p tc">Get in touch with us, we'd love to hear from you.</p>
     </section>
     <contact-form url="/contact" button-text=""></contact-form>
+    <section class="flex-ns justify-between mv5 pt3 bt b--black-30 mw7 mh4 center-ns">
+        <div class="w-50-ns br-ns b--black-30 pt4">
+            <p class="ttu col-p bold-type mb1">Location</p>
+            <p class="col-midgr f5 bold-type mt0">105 台北市松山區南京東路四段183號五樓</p>
+        </div>
+        <div class="w-50-ns pl4-ns pt4">
+            <p class="ttu col-p bold-type mb1">Tel</p>
+            <p class="col-midgr f5 bold-type mt0">+886 287-707-912</p>
+        </div>
+    </section>
 @endsection
