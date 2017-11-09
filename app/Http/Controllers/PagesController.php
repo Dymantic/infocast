f6a817bd50bce6a08559c21ebc7ce7bfc9a373fa
Michael Joyner <joyner.michael@gmail.com>
2017-11-09 12:33:45 +0800

    ordered positions for home page postings

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,7 +9,7 @@ class PagesController extends Controller
 {
     public function home()
     {
-        $postings = Posting::live()->latest()->take(4)->get();
+        $postings = Posting::live()->orderBy('position')->take(4)->get();
         $team = [
             [
                 'name' => 'Shunwen',
