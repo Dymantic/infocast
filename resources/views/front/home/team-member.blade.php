94ae182082d2f1396393f8605ab28e094d102bf1
Michael Joyner <joyner.michael@gmail.com>
2017-11-10 09:51:04 +0800

    team alignment and square profile images

--- resources/views/front/home/team-member.blade.php
+++ resources/views/front/home/team-member.blade.php
@@ -1,9 +1,9 @@
 <div class="w-100 w-50-m w-third-ns flex flex-column justify-between items-center minh-340">
-    <img src="{{ $member['profile'] }}"
-         alt="A profile image of {{ $member['name'] }}"
-         class="w4 br-100"
-    >
     <div class="ph4 tc">
+        <img src="{{ $member['profile'] }}"
+             alt="A profile image of {{ $member['name'] }}"
+             class="w4 h4 br-100"
+        >
         <p class="bold-type f5 mb0">{{ $member['name'] }}</p>
         <p class="bold-type ttu mt1 col-s f5">{{ $member['title'] }}</p>
         <p class="col-pl lh-title">{{ $member['quote'] }}</p>
