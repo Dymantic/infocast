e87c01982b7670266877a1cddf4e444bb0e9831f
Michael Joyner <joyner.michael@gmail.com>
2017-11-11 11:02:49 +0800

    handle no positions situation for home page

--- resources/views/front/home/page.blade.php
+++ resources/views/front/home/page.blade.php
@@ -52,6 +52,7 @@
     </section>
     <section class="pv6">
         <h3 class="normal mt0 tc ttu headf">Available Positions</h3>
+        @if($postings->count() > 0)
         <div class="mw8 flex flex-wrap justify-between center mt5">
             @foreach($postings as $posting)
                 @include('front.home.job-card')
@@ -60,6 +61,12 @@
         <div class="tc mt5">
             <a class="f3 ttu col-p hov-r ba dib center-ns link col-s pv2 ph4 mh3" href="/careers">See all available positions</a>
         </div>
+        @else
+            <p class="f5 f4-ns measure-wide center-ns mh4 lh-copy">We don't have any positions available at the moment. If you really feel like you'd be a good fit on the team, feel free to get in touch and say hi. Who knows what the future holds?</p>
+            <div class="tc mt5">
+                <a class="f3 ttu col-p hov-r ba dib center-ns link col-s pv2 ph4 mh3" href="/contact">Get In Touch</a>
+            </div>
+        @endif
     </section>
 @endsection
 
