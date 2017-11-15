b44d393eeceb999c9144ba3288c33b324f0e80bc
Michael Joyner <joyner.michael@gmail.com>
2017-11-15 09:31:27 +0800

    schedule backup and sitemap tasks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,13 +19,14 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('backup:clean')->dailyAt('01:45');
+        $schedule->command('backup:run')->dailyAt('02:45');
+        $schedule->command('sitemap:generate')->dailyAt('03:45');
     }
 
     /**
@@ -35,7 +36,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
