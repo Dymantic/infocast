9a91cc8746be76d87c86691fe866e766193dca0a
Michael Joyner <joyner.michael@gmail.com>
2017-11-27 10:58:22 +0800

    better grouping of team members in team carousel

--- resources/views/front/home/page.blade.php
+++ resources/views/front/home/page.blade.php
@@ -32,7 +32,7 @@
     </section>
     <section>
         <h3 class="tc ttu headf normal">Meet the Team</h3>
-        <div data-flickity='{"cellAlign": "left", "arrowShape": "M1.11,48.2,80.87.32a2.24,2.24,0,0,1,3.37,2V97.69a2.24,2.24,0,0,1-3.36,2L1.12,52.2A2.34,2.34,0,0,1,1.11,48.2Z", "contain": "true", "draggable": true}'
+        <div data-flickity='{"cellAlign": "left", "arrowShape": "M1.11,48.2,80.87.32a2.24,2.24,0,0,1,3.37,2V97.69a2.24,2.24,0,0,1-3.36,2L1.12,52.2A2.34,2.34,0,0,1,1.11,48.2Z", "contain": "true", "draggable": true, "groupCells": "80%"}'
              class="w-90 w-80-ns mw8 center mv5 pb5 pt4">
             @foreach($team as $member)
                 @include('front.home.team-member')
